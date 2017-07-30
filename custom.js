44ed98a07f271105795e84eb168b3b5df54eb8c1
Abhishek <parmarabhishek.53@gmail.com>
2017-07-30 14:18:57 +0530

    graph1 data retrived

--- custom.js
+++ custom.js
@@ -3,6 +3,7 @@ var completeObjArray = [];
 var firstName = [];
 var rating = [];
 var feedbackFor = [];
+var graph1 = {};
 
 $(document).ready(function(){
 
@@ -19,9 +20,10 @@ $(document).ready(function(){
         
         storeIndividualDetails(completeObjArray);
         
-        console.log(firstName);
-        console.log(feedbackFor);
-        console.log(rating);
+        graph1 = createGraph1Data(feedbackFor);
+        
+        console.log(graph1);/////////////////
+        
     });
     
     
@@ -57,5 +59,19 @@ function storeIndividualDetails(completeObjArray){
         feedbackFor.push(completeObjArray[i].feedback_for);
         rating.push(completeObjArray[i].rating);
     }
+}
 
+function createGraph1Data(feedbackFor){
+    var data = {};
+    for(var i=0;i<feedbackFor.length;i++)
+        {
+            if( Object.keys(data).indexOf(feedbackFor[i]) != -1)
+                {
+                    data[feedbackFor[i]] = data[feedbackFor[i]] + 1;
+                }
+            else{
+                data[feedbackFor[i]] = 1;
+            }
+        }
+    return data;
 }
